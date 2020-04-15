b8878c52b913f7d1a7efa8371431c334d4206cc7
orlanildo <orlanildoaldo@hotmail.com>
2020-04-15 01:39:34 -0300

    layout Login screen with style finalized

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,57 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity, TextInput, Image, KeyboardAvoidingView, Animated } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
-
 import styles from './styles/cssLogin'
+import logoMidium from '../../assets/logoMidium.png'
+
 
 export default function Login() {
     const navigation = useNavigation()
 
+    const [offset] = useState(new Animated.ValueXY({ x: 0, y: 95 }))
+    const [opacity] = useState(new Animated.Value(0))
+
+    useEffect(() => {
+        Animated.parallel([
+            Animated.spring(offset.y, {
+                toValue: 0,
+                speed: 4,
+                bounciness: 20
+            }),
+            Animated.timing(opacity, {
+                toValue: 1,
+                duration: 250,
+            })
+        ]).start()
+    }, [])
+
     return(
-        <View style={styles.container}>
-            <Text>Login</Text>
-            <Button title='go back' onPress={() => navigation.navigate('Register')}/>
-        </View>
+        <KeyboardAvoidingView style={styles.container} >
+            <View style={styles.containerLogoMidium}>
+                <Image style={styles.logoMidium} source={logoMidium} />
+            </View>
+
+            <Animated.View style={[styles.containerInput, {
+                    opacity: opacity,
+                    transform: [{ translateY: offset.y }]
+                }]} >
+
+                <Text style={styles.titleScreen} >Login</Text>
+                <TextInput style={styles.input} placeholder='E-mail' autoCorrect={false}
+                    onChangeText={() => {}} />
+                <TextInput style={styles.input} placeholder='********' autoCorrect={false}
+                    onChangeText={() => {}} />
+
+                <TouchableOpacity style={styles.btnSubmit} >
+                    <Text style={styles.textSubmit} >Enter</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnRegister}>
+                    <Text style={styles.textRegister} onPress={() => navigation.navigate('Register')} >Register</Text>
+                </TouchableOpacity>
+            </Animated.View>
+        </KeyboardAvoidingView> 
     )
 }
 
--- src/pages/styles/cssLogin.js
+++ src/pages/styles/cssLogin.js
@@ -3,7 +3,63 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
     container: {
         flex: 1,
-        alignContent: 'center',
+        alignItems: 'center',
         justifyContent: 'center',
-    }
+        backgroundColor: '#5CFF57'
+    },
+    containerLogoMidium: {
+        marginTop: 80,
+        flex: 2,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+    },
+    logoMidium: {
+        height: 290,
+        width: 250,
+    },
+    titleScreen: {
+        marginTop: -80,
+        color: '#35aaff',
+        fontSize: 18,
+        marginBottom: 10,
+        marginLeft: -260,
+    },
+    containerInput: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+    },
+    input: {
+        backgroundColor: '#fff',
+        width: '90%',
+        marginBottom: 15,
+        fontSize: 18,
+        borderRadius: 7,
+        paddingLeft: 7,
+        height: 45,
+    },
+    textRegister: {
+        color: '#35aaff',
+        marginBottom: 10,
+    },
+    btnSubmit: {
+        backgroundColor: '#35aaff',
+        height: 45,
+        width: '90%',
+        borderRadius: 7,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textSubmit: {
+        color: '#fff',
+        fontSize: 18,
+    },
+    btnRegister: {
+        marginTop: 10,
+    },
+    textRegister: {
+        color: '#35aaff',
+        fontSize: 18,
+    },
 })
